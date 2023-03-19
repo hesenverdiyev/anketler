73fca597b677c0d4615287abf3f1c1f334c87ec8
Hasan Verdiyev <allinone1918@gmail.com>
2023-03-19 15:28:10 +0300

    last commit

--- db.js
+++ db.js
@@ -2,23 +2,17 @@ import mongoose from "mongoose";
 
 mongoose.set('strictQuery', false);
 
-const conn = () => {
-  mongoose.connect(process.env.DB_URI,{
-    dbName: "anketler",
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    connectTimeoutMS: 30000, // 30 seconds
-    reconnectTries: Number.MAX_VALUE, // retry indefinitely
-    reconnectInterval: 1000 // wait 1 second before retrying
-  }).then(()=>{
-    console.log("Connected to the DB successfully");
-  }).catch((err)=>{
-    console.log(`DB connection error: ${err}`);
-    setTimeout(() => {
-      console.log("Attempting to reconnect to the DB...");
-      conn(); // attempt to reconnect
-    }, 1000); // wait 1 second before attempting to reconnect
-  })
+const conn = async () =>{
+    try {
+        await mongoose.connect(process.env.DB_URI,{
+            dbName: "anketler",
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connected to the DB successfully");
+    } catch (error) {
+        console.log(`DB connection error:, ${error}`);
+    }
 };
 
 export default conn;
