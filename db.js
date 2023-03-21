904c41973bf0c33dbe9f37deb8ab2e9fb101a72c
Hasan Verdiyev <allinone1918@gmail.com>
2023-03-21 23:50:37 +0300

    last commit

--- db.js
+++ db.js
@@ -2,21 +2,12 @@ import mongoose from "mongoose";
 
 mongoose.set('strictQuery', false);
 
-// Set appropriate timeouts
-const options = {
-    connectTimeoutMS: 5000,
-    socketTimeoutMS: 30000,
-  };
-
 const conn = async () =>{
     try {
         await mongoose.connect(process.env.DB_URI,{
             dbName: "anketler",
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            maxPoolSize: 10,
-            serverSelectionTimeoutMS: 5000,
-            ...options,
         });
         console.log("Connected to the DB successfully");
     } catch (error) {
